627678ac634238f676582dd2e9dda7710b9de27c
Filip Jukic <filip@jukic.me>
2013-07-28 12:04:27 -0400

    fix za bazu

--- chochek_api/chochek_api/settings.py
+++ chochek_api/chochek_api/settings.py
@@ -1,4 +1,5 @@
 # Django settings for chochek_api project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -12,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'baza.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(os.environ['OPENSHIFT_DATA_DIR'] + 'baza.db'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
